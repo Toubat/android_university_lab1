53e43574c4411311cb3360ac81f2223e46edc479
Toubat <57741529+Toubat@users.noreply.github.com>
2021-02-06 18:16:39 -0500

    not resolved image loading

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -1,12 +1,17 @@
 package com.codepath.bestsellerlistapp;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.codepath.bestsellerlistapp.models.BestSellerBook;
 
 import java.util.List;
@@ -34,9 +39,20 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
 
     @Override
     public void onBindViewHolder(final BookViewHolder holder, int position) {
-        holder.mItem = books.get(position);
-        holder.mBookTitle.setText(books.get(position).title);
-        holder.mBookAuthor.setText(books.get(position).author);
+
+        BestSellerBook book = books.get(position);
+
+        holder.mItem = book;
+        holder.mBookTitle.setText(book.title);
+        holder.mBookAuthor.setText(book.author);
+        holder.mBookRanking.setText(String.valueOf(books.get(position).rank));
+        holder.mBookDescription.setText(book.description);
+
+        Log.i("ABC", book.bookImageUrl);
+        Glide.with(holder.mView)
+                .load(book.bookImageUrl)
+                .centerInside()
+                .into(holder.mBookImage);
 
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -55,17 +71,25 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         return books.size();
     }
 
-    public class BookViewHolder extends RecyclerView.ViewHolder {
+    public static class BookViewHolder extends RecyclerView.ViewHolder {
         public final View mView;
         public final TextView mBookTitle;
         public final TextView mBookAuthor;
+        public final TextView mBookRanking;
+        public final TextView mBookDescription;
+        public final ImageView mBookImage;
+        public final Button mBuyButton;
         public BestSellerBook mItem;
 
         public BookViewHolder(View view) {
             super(view);
             mView = view;
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
+            mBookImage = (ImageView) view.findViewById(R.id.book_image);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+            mBookRanking = (TextView) view.findViewById(R.id.ranking);
+            mBookDescription = (TextView) view.findViewById(R.id.book_description);
+            mBuyButton = (Button) view.findViewById(R.id.buy_button);
         }
 
         @Override
--- app/src/main/java/com/codepath/bestsellerlistapp/MainActivity.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +16,6 @@ public class MainActivity extends AppCompatActivity {
         FragmentManager supportFragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
         fragmentTransaction.replace(R.id.content, new BestSellerBooksFragment(), null).commit();
+
     }
 }
